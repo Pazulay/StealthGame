cf502353826965462066e5c5dd9e4c750866ccc2
Pazulay <azulay3@yahoo.com>
2021-01-11 20:28:17 +0100

    Added file

---
+++ StealthGame/Assets/Destination.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Destination : MonoBehaviour
+{
+    public GameObject finishScreen;
+    bool isGameDone = false;
+    GameObject player;
+    
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+   
+    void Update()
+    {
+        StartGameAgain();
+    }
+
+    void OnTriggerStay(Collider collider) {
+        if (collider.gameObject.tag == "Player" && Vector2.Distance(transform.position, player.transform.position) < 4){
+            finishScreen.SetActive(true);
+            isGameDone = true;
+            
+
+        }
+    }
+
+    void StartGameAgain(){
+        if (Input.GetKeyDown(KeyCode.Space) && isGameDone){
+            SceneManager.LoadScene("SampleScene");
+        }
+
+    }
+}
---
+++ StealthGame/Assets/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 7;
+    public float smoothMoveTime = .1f;
+    public float turnSpeed = 8;
+
+    float smoothInputMagnitude;
+    float smoothMoveVelocity;
+    float angle;
+    Vector3 velocity;
+    
+    Rigidbody rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    
+    void Update()
+    {
+        Vector3 inputDirection = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical")).normalized;
+		float inputMagnitude = inputDirection.magnitude;
+        smoothInputMagnitude = Mathf.SmoothDamp (smoothInputMagnitude, inputMagnitude, ref smoothMoveVelocity, smoothMoveTime);
+
+        float targetAngle = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg;
+        angle = Mathf.LerpAngle(angle, targetAngle, Time.deltaTime * turnSpeed * inputMagnitude);
+
+        velocity = transform.forward * speed * smoothInputMagnitude;
+    }
+
+    void FixedUpdate() {
+        rb.MoveRotation(Quaternion.Euler(Vector3.up * angle));
+        rb.MovePosition(rb.position + velocity * Time.deltaTime);
+    }
+}
